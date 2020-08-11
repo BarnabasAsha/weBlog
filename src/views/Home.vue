8174c444d9b8c51a066cfd5d9829f80abc4b3d74
Barnabas Asha <barnabee58@gmail.com>
2020-08-11 13:12:31 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Welcome to Wejapa Blog</h1>
+   <p>Below are the recent posts</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
